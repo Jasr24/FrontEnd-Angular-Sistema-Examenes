9105c5c062a0cf3a755438e3795f34a6f1083604
Andres <josex.1995@hotmail.com>
2022-10-05 16:33:53 -0500

    Agregando validaciones al formulairo de iniciar seccion 'login'

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,3 +1,4 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginData = {
+    "username" : "",
+    "password" : ""
+  }
+
+  constructor(private snack:MatSnackBar) { }
 
   ngOnInit(): void {
+  }
+
+  formSubmit(){
+    if(this.loginData.username.trim() == null || this.loginData.username.trim() == ''){
+      this.snack.open('El nombre de usuario es requerido!!' , 'Aceptar',{
+        duration:3000
+      })
+      return;
+    }
+
+    if(this.loginData.password.trim() == null || this.loginData.password.trim() == ''){
+      this.snack.open('La contraseña es requerida!!' , 'Aceptar',{
+        duration:3000
+      })
+      return;
+    }
   }
 
 }
