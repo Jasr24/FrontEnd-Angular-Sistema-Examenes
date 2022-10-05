904128389ffa5be3d5aba0f3509a7cd8464506ab
Andres <josex.1995@hotmail.com>
2022-10-05 17:56:34 -0500

    Agregando metodos a la clase login.service.ts, metodos loginUser.. logaut entre otros NOTA: en el backend agregamos el metodo ObtenerUsuarioActual()

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -13,4 +13,51 @@ export class LoginService {
   public generateToken(loginData:any){
     return this.http.post(`${baseUrl}/generate-token`,loginData); //este loginData actua como el reques body
   }
+
+  //Iniciamos sesión y establecemos el token en el localStorage, osea guarderemos el token en el localStorage en el navegador para que este hay por sierto tiempo.
+  public loginUser(token:any){
+    localStorage.setItem('token',token)
+  }
+
+  //Este metodo nos sirve para comprobar si esta loggedo o no.
+  public isLoggedIn(){
+    let tokenStr = localStorage.getItem('token');
+    if(tokenStr == null || tokenStr == undefined || tokenStr == ''){
+      return false;
+    }else{
+      return true;
+    }
+  }
+
+  //Cerramos cession y eliminamos el token del localStorage
+  public logout(){
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    return true;
+  }
+
+  //Pbtener el tokenStr
+  public getToken(){
+    return localStorage.getItem('token');
+  }
+
+  public setUser(user:any){
+    localStorage.setItem('user', JSON.stringify(user)); //De JSON a String
+  }
+
+  public getUser(){
+    let userStr = localStorage.getItem('user');
+    if(userStr != null){
+      return JSON.parse(userStr); //De String a JSON
+    } else {
+      this.logout();
+      return null;
+    }
+  }
+
+  public getUserRole(){
+    let user = this.getUser();
+    return user.authorities[0].authority;
+  }
+
 }
